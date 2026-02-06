883298c7b4d29c9e7d82348ce04fe483f6bb22f9
rastin-ghasemi <rastinghasemi5@gmail.com>
2026-02-06 14:37:43 +0200

    ECS

--- Terraform/Deploy/Database.tf
+++ Terraform/Deploy/Database.tf
@@ -13,39 +13,42 @@ resource "aws_db_subnet_group" "main" {
 }
 
 resource "aws_security_group" "rds" {
-    description = "Allow access to the RDS database instance"
-    name = "${local.Prefix}-rds-inbound-access"
-    vpc_id = aws_vpc.main.id
+  description = "Allow access to the RDS database instance"
+  name        = "${local.Prefix}-rds-inbound-access"
+  vpc_id      = aws_vpc.main.id
 
-    ingress {
-    protocol = "tcp"
+  ingress {
+    protocol  = "tcp"
     from_port = 5432
-    to_port = 5432
-    }
+    to_port   = 5432
+    security_groups = [
+      aws_security_group.ecs_service.id
+    ]
+  }
 
-    tags = {
+  tags = {
     Name = "${local.Prefix}-db-security-group"
-    }
+  }
 }
 
 resource "aws_db_instance" "main" {
-    identifier = "${local.Prefix}-db"
-    db_name = "recipe"
-    allocated_storage = 20
-    storage_type = "gp2"
-    engine = "postgres"
-    engine_version = "15.3"
-    auto_minor_version_upgrade = true
-    instance_class = "db.t4g.micro"
-    username = var.db_username
-    password = var.db_password
-    skip_final_snapshot = true
-    db_subnet_group_name = aws_db_subnet_group.main.name
-    multi_az = false
-    backup_retention_period = 0
-    vpc_security_group_ids = [aws_security_group.rds.id]
+  identifier                 = "${local.Prefix}-db"
+  db_name                    = "recipe"
+  allocated_storage          = 20
+  storage_type               = "gp2"
+  engine                     = "postgres"
+  engine_version             = "15.3"
+  auto_minor_version_upgrade = true
+  instance_class             = "db.t4g.micro"
+  username                   = var.db_username
+  password                   = var.db_password
+  skip_final_snapshot        = true
+  db_subnet_group_name       = aws_db_subnet_group.main.name
+  multi_az                   = false
+  backup_retention_period    = 0
+  vpc_security_group_ids     = [aws_security_group.rds.id]
 
-    tags = {
+  tags = {
     Name = "${local.Prefix}-main"
-    }
+  }
 }
--- Terraform/Deploy/var.tf
+++ Terraform/Deploy/var.tf
@@ -29,10 +29,22 @@ variable "vpc_cidr" {
 
 variable "db_username" {
   description = "User for RDS DB."
-  default = "recipeapp"
-  
+  default     = "recipeapp"
+
 }
 variable "db_password" {
   description = "Password for RDS DB"
 
+}
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django"
 }
