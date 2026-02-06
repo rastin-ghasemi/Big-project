239c0445c2a55a87d89205fd57b4192c92432081
rastin-ghasemi <rastinghasemi5@gmail.com>
2026-02-06 16:56:34 +0200

    ECS

--- Terraform/Deploy/var.tf
+++ Terraform/Deploy/var.tf
@@ -1,6 +1,6 @@
 variable "Prefix" {
   description = "The name of project"
-  default     = "Deploy-Project"
+  default     = "deploy-project"
 }
 variable "Project" {
   description = "The name of project"
@@ -11,4 +11,40 @@ variable "Project" {
 variable "contact" {
   description = "Who Responsible for this Infra"
   default     = "rastinghasemi5@gmail.com"
+}
+variable "terraform-state-locking" {
+  description = "TF state Bucket Locking"
+  default     = "terraform-state-locking"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+  validation {
+    condition     = can(cidrhost(var.vpc_cidr, 0))
+    error_message = "Must be a valid CIDR block."
+  }
+}
+
+variable "db_username" {
+  description = "User for RDS DB."
+  default     = "recipeapp"
+
+}
+variable "db_password" {
+  description = "Password for RDS DB"
+
+}
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django"
 }
