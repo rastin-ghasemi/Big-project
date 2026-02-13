fd39afc10eb312045842a8c32b4cc5b2cb9b1a22
rastin-ghasemi <rastinghasemi5@gmail.com>
2026-02-13 13:41:32 +0200

    HTTPS

--- Terraform/Deploy/ECS.tf
+++ Terraform/Deploy/ECS.tf
@@ -207,7 +207,7 @@ resource "aws_security_group" "ecs_service" {
     protocol  = "tcp"
     cidr_blocks = [
       aws_subnet.Private-a.cidr_block,
-      aws_subnet.Private-a.cidr_block
+      aws_subnet.Private-b.cidr_block
     ]
   }
 
@@ -230,7 +230,7 @@ resource "aws_ecs_service" "api" {
   task_definition        = aws_ecs_task_definition.api.family
   desired_count          = 1
   launch_type            = "FARGATE"
-  platform_version       = "1.6.0"
+  platform_version       = "LATEST"
   enable_execute_command = true
 
   network_configuration {
